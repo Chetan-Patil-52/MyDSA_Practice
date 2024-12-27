84584702d892fd32e7e480229070a190e6528ede
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2024-12-28 02:29:30 +0530

    Update 16. 3Sum Closest.cpp

--- Arrays/16. 3Sum Closest.cpp
+++ Arrays/16. 3Sum Closest.cpp
@@ -4,19 +4,22 @@ public:
         int n = nums.size();
         int closestsum = 100000;
         sort(nums.begin(),nums.end());
-        for(int k =0;k<=n-3;k++){
+        for(int k =0;k<n-2;k++){
             int i = k+1,j=n-1;
-            int sum = nums[k] + nums[i] + nums[j];
-            if(abs(target-sum)<abs(target-closestsum)){
+            while(i<j){
+                int sum = nums[k] + nums[i] + nums[j];
+                if(abs(target-sum)<abs(target-closestsum)){
                 closestsum = sum;
-            }
+                }
 
-            if(sum < target){
-                i++;
-            }else{
-                j--;
+                if(sum < target){
+                    i++;
+                }else{
+                    j--;
+                }
             }
         }
         return closestsum;
     }
 };
+
