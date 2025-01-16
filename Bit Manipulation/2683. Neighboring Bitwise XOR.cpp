4b8a2e1318931d8493d49dacb4015c1c0651d3c4
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-01-16 20:01:43 +0530

    Update 2683. Neighboring Bitwise XOR.cpp

--- Bit Manipulation/2683. Neighboring Bitwise XOR.cpp
+++ Bit Manipulation/2683. Neighboring Bitwise XOR.cpp
@@ -1,27 +1,46 @@
+// class Solution {
+// public:
+//     bool doesValidArrayExist(vector<int>& derived) {
+//         int n= derived.size();
+//         vector<int> original(n);
+//         // let original[i]=0
+//         original[0] = 0;
+//         for(int i=0;i<n-1;i++){
+//             original[i+1] = original[i] ^ derived[i];
+//         }
+
+//         if(original[n-1] ^ original[0] == derived[n-1]){
+//             return true;
+//         }
+
+//         // let original[i]=1
+//         original[0] = 1;
+//         for(int i=0;i<n-1;i++){
+//             original[i+1] = original[i] ^ derived[i];
+//         }
+
+//         if(original[n-1] ^ original[0] == derived[n-1]){
+//             return true;
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        int n= derived.size();
-        vector<int> original(n);
-        // let original[i]=0
-        original[0] = 0;
-        for(int i=0;i<n-1;i++){
-            original[i+1] = original[i] ^ derived[i];
+        // Big Brain Approach 
+
+        int xOR = 0;
+        for(int num : derived){
+            xOR ^= num;
         }
 
-        if(original[n-1] ^ original[0] == derived[n-1]){
+        if(xOR==0){
             return true;
+        }else{
+            return false;
         }
-
-        // let original[i]=1
-        original[0] = 1;
-        for(int i=0;i<n-1;i++){
-            original[i+1] = original[i] ^ derived[i];
-        }
-
-        if(original[n-1] ^ original[0] == derived[n-1]){
-            return true;
-        }
-        return false;
     }
 };
