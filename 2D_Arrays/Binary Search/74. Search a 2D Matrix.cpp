e76f4aa4856b0dabc0c6e9e798fb5e060cceb57c
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-03-16 20:04:54 +0530

    Create 74. Search a 2D Matrix.cpp

---
+++ 2D_Arrays/Binary Search/74. Search a 2D Matrix.cpp
@@ -0,0 +1,46 @@
+// Without Binary Search 
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int i=0,j=n-1;
+
+        while(i < m && j >= 0){
+            if(matrix[i][j] > target){
+                j--;
+            }else if(matrix[i][j] < target){
+                i++;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// With BS
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int start = 0;
+        int end = (n * m)-1;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+
+            if(matrix[mid/n][mid%n] < target){
+                start = mid + 1;
+            }else if(matrix[mid/n][mid%n] > target){
+                end = mid - 1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
