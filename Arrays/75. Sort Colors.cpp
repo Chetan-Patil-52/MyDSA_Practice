56875c1ab3245b88b026fa36028e040bd70d44d0
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-06-24 14:59:43 +0530

    Update 75. Sort Colors.cpp

--- Arrays/75. Sort Colors.cpp
+++ Arrays/75. Sort Colors.cpp
@@ -1,3 +1,26 @@
+// incplace in O(n) or O(2n)
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        vector<int> result;
+        int arr[10] = {0};
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            arr[nums[i]]++;
+        }
+        int count = 0;
+        for(int i=0;i<10;i++){
+            int freq = arr[i];
+            for(int j=0;j<freq;j++){
+                nums[count] = i;
+                count++;
+            }
+            
+        }
+        // return result;
+    }
+};
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
