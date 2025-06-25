faa20d4907a26c14828b9bb19e83de61dff2ccfa
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-06-25 14:53:50 +0530

    Update 334. Increasing Triplet Subsequence.cpp

--- Arrays/334. Increasing Triplet Subsequence.cpp
+++ Arrays/334. Increasing Triplet Subsequence.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
+        // Using DP 
+        int n= nums.size();
+        vector<int> t(n,1);
+
+        int max_val = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    t[i] = max(t[i],t[i]+1);
+                }
+                
+            max_val = max(max_val, t[i]);
+            if(max_val >= 3) break;
+            }
+
+            
+        }
+        return max_val>=3?true:false;
+    }
+};
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
         int num1=INT_MAX,num2=INT_MAX;
         for(int &num3 : nums){
             if(num3<=num1){
