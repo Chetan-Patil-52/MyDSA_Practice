1c39e47bacc24540ac137673d178a8d8f923d897
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-06-27 17:01:37 +0530

    Create Sky diving.cpp

---
+++ Amazon Interview Preparation/ Amazon Coding Assessment/Sky diving.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int min_parachutes(int k, int n) {
+    // Write your logic here.
+    return 0;
+}
+
+int main() {
+    int n, k;
+    std::cin >> n >> k;
+    
+    // Call user logic function and print the output
+    int result = min_parachutes(k, n);
+    std::cout << result << std::endl;
+    return 0;
+}
