4818673d5eed178dca2667dba9806b03bf080ff2
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-07-05 17:43:28 +0530

    Update 1394. Find Lucky Integer in an Array.cpp

--- Easy/1394. Find Lucky Integer in an Array.cpp
+++ Easy/1394. Find Lucky Integer in an Array.cpp
@@ -47,3 +47,30 @@ public:
         return maxFreq;
     }
 };
+
+// Crazy Approach using bit manipulation 
+//Approach-3 (Using bits to store frequency)
+//T.C : O(n)
+//S.C : O(1) in place
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        int n = arr.size();
+
+        for(int &num : arr) {
+            int val = (num & 65535);
+
+            if(val <= n) {
+                arr[val-1] += (1<<16); //65536, pow(2, 16)
+            }
+        }
+
+        for(int val = n; val >= 1; val--) {
+            if((arr[val-1] >> 16) == val) {
+                return val;
+            }
+        }
+
+        return -1;
+    }
+};
