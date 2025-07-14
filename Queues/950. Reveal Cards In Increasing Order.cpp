b6fff205eee26678294f84c6a61b3dddb05a9607
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-07-14 21:03:29 +0530

    Update 950. Reveal Cards In Increasing Order.cpp

--- Queues/950. Reveal Cards In Increasing Order.cpp
+++ Queues/950. Reveal Cards In Increasing Order.cpp
@@ -24,3 +24,33 @@ public:
         return result;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        int n= deck.size();
+        vector<int> result(n);
+        queue<int> que;
+
+        for(int i=0;i<n;i++){
+            que.push(i);
+        }
+
+        sort(begin(deck),end(deck));
+
+        for(int i=0;i<n;i++){
+            int idx = que.front();
+            que.pop();
+
+            result[idx] = deck[i];
+
+            if(!que.empty()){
+                que.push(que.front());
+                que.pop();
+            }
+
+        }
+        return result;
+    }
+};
