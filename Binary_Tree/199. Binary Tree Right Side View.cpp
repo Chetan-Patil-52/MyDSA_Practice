a3deee09e3ab15ef08a446ea8a11c1ed91a6b475
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2026-04-19 20:11:26 +0530

    Create 199. Binary Tree Right Side View.cpp

---
+++ Binary_Tree/199. Binary Tree Right Side View.cpp
@@ -0,0 +1,59 @@
+// BFS 
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if(root == NULL) return {};
+        vector<int> result;
+        queue<TreeNode*> que;
+        que.push(root);
+        while(!que.empty()){
+            int n = que.size();
+            TreeNode* node = NULL;
+            while(n--){
+                node = que.front();
+                que.pop();
+                if(node->left != NULL){
+                    que.push(node->left);
+                }
+                if(node->right != NULL){
+                    que.push(node->right);
+                }
+            }
+            result.push_back(node->val);
+        }
+        return result;
+    }
+};
+
+// DFS
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void preOrder(TreeNode* root,int level,vector<int>& result){
+        if(root == NULL) return;
+        int n = result.size();
+        if(n < level){
+            result.push_back(root->val);
+        }
+        preOrder(root->right,level+1,result);
+        preOrder(root->left,level+1,result);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        preOrder(root,1,result);
+        return result;
+    }
+};
+
